237824fd1c66aa976dc3ab26265830b71cd1cd90
ljc <834775778@qq.com>
2020-05-22 18:39:54 +0800

    k

--- src/common/script/config.js
+++ src/common/script/config.js
@@ -5,5 +5,13 @@ const Host = {
 module.exports = {
     Urls: {
         
-    }
+    },
+
+    //网页上方导航栏
+    TopNavList: [
+    	{title: '首页', icon:'', name:'index', path:'/home'},
+    	{title: '直播', icon:'', name:'live-show', path:'/home'},
+    	{title: '视频', icon:'', name:'video-show', path:'/home'},
+    	{title: '关于', icon:'', name:'about-us', path:'/home'}
+    ]
 }
--- src/components/TopNav/TopNav.vue
+++ src/components/TopNav/TopNav.vue
@@ -4,16 +4,41 @@
 
 <template>
 	<div class="top-nav-container">
-		
+		<Menu mode="horizontal" :theme="theme" :style="{width: width}">
+			<template v-for="(item, index) in TopNavList">
+				<MenuItem :name="index" :key="item.name">
+                	{{ item.title }}
+            	</MenuItem>
+			</template>
+		</Menu>
+		<div class="search-box">
+			<Input type="text" />
+		</div>
 	</div>
 </template>
 
 <script>
 import Action from './action/TopNav';
+import { TopNavList } from '../../common/script/config';
 export default {
+	props:{
+    	theme: {
+    		type: String,
+    		default: 'light'
+    	},
+    	width: {
+    		type: String,
+    		default: '16rem'
+    	}
+    },
 	data () {
 		return {
 			
+		}
+	},
+	computed: {
+		TopNavList () {
+			return TopNavList;
 		}
 	}
 }
